e77ba96dd02e5bb304e0c9137f66f377fb37ed35
Rocco Lo Re <rocco.lore@branchina.edu.it>
2026-04-09 13:59:32 +0200

    modified cors

--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,15 @@ const port: number = 3000;
 
 async function start() {
   await runMigrations(); // esegui prima di tutto
+  
+  const corsOptions = {
+  origin: 'https://hof.branchina.edu.it', // Permette solo questo dominio
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Metodi consentiti
+  allowedHeaders: ['Content-Type', 'Authorization'], // Header accettati
+  optionsSuccessStatus: 200 // Per compatibilità con vecchi browser (es. IE11)
+  };
 
-  app.use(cors());
+  app.use(cors(corsOptions));
   app.use(express.json());
 
   app.use('/api/progetti', progettiRoutes);
